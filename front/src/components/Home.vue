400f134982e172397de1804b2890c6c208afa9c7
Petrus Nogueira <petrusyagami@gmail.com>
2020-12-13 17:06:09 -0300

    Homepage interface

---
+++ front/src/components/Home.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="home container pl-4 pr-4 is-flex is-flex-direction-column is-justify-content-center is-align-items-center">
+      <img alt="Instalker logo" src="../assets/instalker-logo.png">
+      <h1 class="is-size-1 mt-3">In<strong>Stalker</strong></h1>
+      <VueTyper
+      :text='["Stalk people on Instagram like a PRO.", "Who are you stalking today?", "Why are you stalking people anyway? Weirdo.", "What about stalking @natalieportman today?"]'
+      :repeat='Infinity'
+      :shuffle='false'
+      initial-action='typing'
+      :pre-type-delay='70'
+      :type-delay='30'
+      :pre-erase-delay='2000'
+      :erase-delay='250'
+      erase-style='select-all'
+      :erase-on-complete='false'
+      caret-animation='blink'>
+      </VueTyper>
+      <Searchform></Searchform>
+    </div>
+</template>
+
+<script>
+    var VueTyper = window.VueTyper.VueTyper
+    import Searchform from './Searchform.vue'
+    export default {
+        components: {
+          VueTyper,
+          Searchform
+        }
+    }
+</script>
+
+<style>
+
+  .home{
+    height: 100%;
+    margin-top: -40px;
+  }
+
+</style>
---
+++ front/src/components/Searchform.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="is-flex mt-5">
+        <div class="control has-icons-left mr-3 is-medium">
+            <input type="text" placeholder="milliebobbybrown" class="input is-medium is-hovered"/>
+            <span class="icon is-left">
+                <i class="fas fa-at"></i>
+            </span>
+        </div>
+        <button class="button is-hovered is-medium" type="button" name="button"><i class="fas fa-search"></i></button>
+    </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css" scoped>
+</style>
